7eb5bc7f48ba100c6e618de322cb314d7ca8656a
lucasrvcintra <lucasrcintra@hotmail.com>
2024-06-17 21:48:10 -0300

    aprendendo CRUD

--- node/server.js
+++ node/server.js
@@ -1,9 +1,53 @@
-import { createServer } from "node:http";
+// import { createServer } from "node:http";
 
-const server = createServer((request, response) => {
-  console.log("oi");
+// const server = createServer((request, response) => {
+//   response.write("oi");
+//   return response.end();
+// });
 
-  return response.end();
+// server.listen(3333);
+
+import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
+
+const server = fastify();
+
+const database = new DatabaseMemory();
+
+// CRUD - Create, Read, Update, Delete
+
+// GET, POST, PUT, DELETE, PATCH
+
+// POST http://localhost:3333/videos
+
+// Route paremeter
+
+server.post("/videos", (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  database.create({
+    title,
+    description,
+    duration,
+  });
+
+  return reply.status(201).send();
 });
 
-server.listen(3333);
+server.get("/videos", () => {
+  return "Hello Rocket";
+});
+
+// PUT http://localhost:3333/videos/3
+
+server.put("/videos/:id", () => {
+  return "Hello Node";
+});
+
+server.delete("/videos/:id", () => {
+  return "Hello Delete";
+});
+
+server.listen({
+  port: 3333,
+});
